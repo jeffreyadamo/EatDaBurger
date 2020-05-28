029f3fc39e2c9f130f57c77c31025fb265631c55
jeffreyadamo <jeffreyadamo@gmail.com>
2020-05-28 14:16:12 -0700

    DB setup

--- db/schema.sql
+++ db/schema.sql
@@ -2,12 +2,12 @@ DROP DATABASE IF EXISTS burgers_db;
 
 CREATE DATABASE burgers_db;
 
-USE burgers_sb;
+USE burgers_db;
 
 CREATE TABLE burgers (
     id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(100) NOT NULL,
-    devoured BOOLEAN,
+    devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
 
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Explosivo', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Inversion', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BasicB', false);
