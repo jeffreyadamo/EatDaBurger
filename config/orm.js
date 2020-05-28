7329326f53c76bfea80b58f1a51a26e657d4fea6
jeffreyadamo <jeffreyadamo@gmail.com>
2020-05-28 14:30:28 -0700

    Config setup

---
+++ config/orm.js
@@ -0,0 +1,19 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+function selectAll(){
+
+}; //close selectAll
+
+function insertOne(){
+    
+}; //close insertOne
+
+function updateOne(){
+
+}; //close updateOne
+
+var orm = {};
+
+// Export the orm object for the model (burgers.js).
+module.exports = orm;
