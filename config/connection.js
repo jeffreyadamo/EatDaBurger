8b461073d228747db2514deb1f7f15cd54fba63e
jeffreyadamo <jeffreyadamo@gmail.com>
2020-05-31 14:09:51 -0700

    setting up MYSQLHeroku

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,27 @@
 var mysql = require("mysql");
 require("dotenv").config();
 
-var connection = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: "burgers_db",
-});
+// var connection = mysql.createConnection({
+//   host: process.env.MYSQL_HOST,
+//   port: process.env.MYSQL_PORT,
+//   user: process.env.MYSQL_USER,
+//   password: process.env.MYSQL_PASSWORD,
+//   database: "burgers_db",
+// });
+
+var connection;
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+  connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: "burgers_db"
+  });
+};
+
 
 // Make connection.
 connection.connect(function (err) {
